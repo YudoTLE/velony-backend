94665e2889d0215afca8fce9971fbc214f25e2d5
YudoTLE <yudojago@gmail.com>
2025-10-05 11:10:04 +0700

    feat: add global validation pipe with configuration options

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -6,6 +7,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      stopAtFirstError: false,
+    }),
+  );
   app.setGlobalPrefix('api');
   const port = configService.get<number>('PORT')!;
   await app.listen(port);
