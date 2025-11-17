bae06adb3ef54ba0dda2857ddd529e1858dd656a
YudoTLE <yudojago@gmail.com>
2025-11-18 06:46:45 +0700

    feat: implement user and conversation modules with controllers and services; add DTOs for user and conversation details and summaries

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { validate } from './config/env.validation';
-import { ConversationModule } from './conversations/conversation.module';
-import { ConversationService } from './conversations/conversation.service';
+import { ConversationModule } from './conversation/conversation.module';
+import { ConversationService } from './conversation/conversation.service';
 import { DatabaseModule } from './database/database.module';
 import { MailModule } from './mail/mail.module';
 import { RedisModule } from './redis/redis.module';
 import { StorageModule } from './storage/storage.module';
-import { UserModule } from './users/user.module';
+import { UserModule } from './user/user.module';
 import { VerificationModule } from './verification/verification.module';
 import { VerificationService } from './verification/verification.service';
 
